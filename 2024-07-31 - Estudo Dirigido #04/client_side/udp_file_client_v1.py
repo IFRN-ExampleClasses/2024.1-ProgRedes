68cdc4f3ca34599029d856c1d44d141df4d47b7f
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2024-07-31 15:05:24 -0300

    SOCKETS
    Atualizando os exemplos do dia 08/07/2024 e fazendo upload dos exemplos de um UDP FILE SERVER.

---
+++ 2024-07-31 - Estudo Dirigido #04/client_side/udp_file_client_v1.py
@@ -0,0 +1,19 @@
+import socket
+
+SERVER = '127.0.0.1'
+PORT   = 31435
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, 25000)
+
+while True:
+    strNomeArq = input('Nome do arquivo a fazer download: ')
+    sock.sendto(strNomeArq.encode('utf-8'), (SERVER, PORT))
+
+    data, addr = sock.recvfrom(4096)
+
+    fd = open (strNomeArq, 'wb')
+    fd.write (data)
+    fd.close()
+
+sock.close()
