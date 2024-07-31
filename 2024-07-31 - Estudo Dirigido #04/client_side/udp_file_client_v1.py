4b4ed87bd18b8ce7df9a5967ee5ab1f3c9f10173
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2024-07-31 15:55:42 -0300

    SOCKETS
    Correção dos exemplos do UDP_FILE_SERVER

--- 2024-07-31 - Estudo Dirigido #04/client_side/udp_file_client_v1.py
+++ 2024-07-31 - Estudo Dirigido #04/client_side/udp_file_client_v1.py
@@ -4,7 +4,7 @@ SERVER = '127.0.0.1'
 PORT   = 31435
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, 25000)
+#sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, 25000)
 
 while True:
     strNomeArq = input('Nome do arquivo a fazer download: ')
--- 2024-07-31 - Estudo Dirigido #04/server_side/udp_file_server_v1.py
+++ 2024-07-31 - Estudo Dirigido #04/server_side/udp_file_server_v1.py
@@ -1,28 +1,28 @@
 import socket
 
 INTERFACE = '127.0.0.1'
-PORT = 31435
+PORT      = 31435
 
 sock = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((INTERFACE, PORT))
 
-print ("Escutando em ...", (INTERFACE, PORT))
+print ('Escutando em ...', (INTERFACE, PORT))
 
 while True:
     # Recebe o nome do arquivo a servir
-    data, source = sock.recvfrom (512)
+    data, source = sock.recvfrom(512)
 
     # Abre o arquivo a servir ao cliente
-    fileName = data.decode('utf-8')
-
-    print ("Recebi pedido para o arquivo ", fileName)
-    fd = open (fileName, 'rb')
+    strFileName = data.decode('utf-8')
+    print ('Recebi pedido para o arquivo ', strFileName)
+    fd = open (strFileName, 'rb')
 
     # Lê o conteúdo do arquivo a enviar ao cliente
-    print ("Enviando arquivo ", fileName)
+    print ('Enviando arquivo ...', strFileName)
     fileData = fd.read(4096)
     sock.sendto(fileData, source)
 
     # Fecha o arquivo
     fd.close()
+
 sock.close()
